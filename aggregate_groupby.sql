cfc2d1d6eda232a6e58baddab21f0ca02f819b8c
Yogananthini A <yogananthinia@gmail.com>
2026-07-09 14:33:45 +0530

    aggregate_groupby.sql

--- aggregate_groupby.sql
+++ aggregate_groupby.sql
@@ -1,16 +1,21 @@
+-- count all rows in employees table
 SELECT COUNT(*) FROM Employees;
 
+-- calculate average salary
 SELECT AVG(salary) AS avg_salary
 FROM Employees;
 
+-- count employees for each department
 SELECT department, COUNT(*) AS total_employees
 FROM Employees
 GROUP BY department;
 
+-- max salary for each department
 SELECT department, MAX(salary) AS highest_salary
 FROM Employees
 GROUP BY department;
 
+-- Select department which is having more than one employee
 SELECT department, COUNT(*)
 FROM Employees
 GROUP BY department
