7d81bbfa405d380f8e2eee15b87c15bb108f57df
Yogananthini A <yogananthinia@gmail.com>
2026-07-09 14:37:27 +0530

    window_functions.sql

--- window_functions.sql
+++ window_functions.sql
@@ -1,13 +1,16 @@
+-- using rank()
 SELECT name,
        salary,
        RANK() OVER (ORDER BY salary DESC) AS rnk
 FROM Employees;
 
+-- using dense rank()
 SELECT name,
        salary,
        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
 FROM Employees;
 
+-- CTE with window function
 WITH emp_table AS (
     SELECT name,
            salary,
