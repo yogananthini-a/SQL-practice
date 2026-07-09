f461e9b7642c7800effc83e8ad0f55f3f0ddf556
Yogananthini A <yogananthinia@gmail.com>
2026-07-09 14:36:18 +0530

    joins_subqueries.sql

--- joins_subqueries.sql
+++ joins_subqueries.sql
@@ -1,15 +1,18 @@
+-- using inner join
 SELECT Employees.name,
        Departments.department
 FROM Employees
 INNER JOIN Departments
 ON Employees.dept_id = Departments.dept_id;
 
+-- using left join
 SELECT Employees.name,
        Departments.department
 FROM Employees
 LEFT JOIN Departments
 ON Employees.dept_id = Departments.dept_id;
 
+-- subquery
 SELECT name
 FROM Employees
 WHERE salary = (
