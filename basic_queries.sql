7d15551736f34de19cd5e1e47a6133405ddc3671
Yogananthini A <yogananthinia@gmail.com>
2026-07-09 14:35:16 +0530

    basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -1,9 +1,12 @@
+-- select all rows on orders table
 SELECT * FROM Orders;
 
+-- return rows which contains amount greater than 1000
 SELECT *
 FROM Orders
 WHERE amount > 1000;
 
+-- calculate the total amount for each year
 SELECT YEAR(order_date), SUM(amount) AS Total_Sales
 FROM Orders
 GROUP BY YEAR(order_date);
